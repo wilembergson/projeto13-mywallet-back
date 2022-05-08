cf80665f145a21248d6c1a17deea3d28ee71f075
wilembergson <wilembergson@yahoo.com>
2022-05-08 17:53:27 -0300

    Operations partial

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getUsers, signIn, signUp } from "../controllers/AuthController.js";
+import { getOperations, Operation } from "../controllers/OperationsController.js";
 import { signInValidation, signUpValidation } from "../middlewares/AuthMiddleware.js";
 
 const userRouter = Router()
@@ -7,5 +8,7 @@ const userRouter = Router()
 userRouter.post("/signup", signUpValidation, signUp)
 userRouter.post("/signin", signInValidation, signIn)
 userRouter.get("/users", getUsers)
+userRouter.post("/operation", Operation)
+userRouter.get("/operations", getOperations)
 
 export default userRouter
